b62b9cac926a58ab1377a1a1c77f247ab8be0b79
Alexandre Speltri <alexandreporks@gmail.com>
2024-11-24 13:49:27 -0300

    Att DB props

--- src/main/java/com/galega/order/adapters/AppConfig.java
+++ src/main/java/com/galega/order/adapters/AppConfig.java
@@ -1,20 +1,16 @@
 package com.galega.order.adapters;
 
 import lombok.Getter;
-import lombok.Setter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Getter
-@Setter
 @Component
-@ConfigurationProperties(prefix = "aws")
 public class AppConfig {
 
-	private String region; //Fila onde as mensagens serão publicadas
-	private String sqsOutputQueueUrl; //Fila que escuta o SNS
-	private String sqsInputQueueUrl;
-	private String accessKeyId;
-	private String secretKey;
-	private String sessionToken;
+	private final String region = System.getenv("AWS_REGION");
+	private final String sqsOutputQueueUrl = System.getenv("AWS_SQS_OUTPUT_QUEUE_URL");; //Fila onde as mensagens serão publicadas
+	private final String sqsInputQueueUrl= System.getenv("AWS_SQS_INPUT_QUEUE_URL"); //Fila que escuta o SNS
+	private final String accessKeyId= System.getenv("AWS_ACCESS_KEY_ID");
+	private final String secretKey= System.getenv("AWS_SECRET_ACCESS_KEY");
+	private final String sessionToken= System.getenv("AWS_SESSION_TOKEN");
 }
